9a55eaab9e6a766b95d14b0f05a87a0253b75c7f
xvoorvaa <xander@xvproductions.com>
2024-03-28 11:55:29 +0100

    Added better time and host management

--- src/host.c
+++ src/host.c
@@ -7,33 +7,37 @@
 #include <string.h>
 #include <sys/socket.h>
 
-const char *fetchHostname(const char *hostname) {
-  struct addrinfo hints, *result;
-  char *ip_str = malloc(NI_MAXHOST);
+#include "ft_ping.h"
 
-  if (!ip_str) {
-    perror("malloc() error");
-    exit(1);
-  }
+const char *fetchHostname(const char *hostname) {
+  t_addrinfo hints, *result;
 
   memset(&hints, 0, sizeof(hints));
+
   hints.ai_family = PF_INET;
   hints.ai_socktype = SOCK_RAW;
 
   int32_t ret = getaddrinfo(hostname, NULL, &hints, &result);
-  if (ret < 0) {
-    perror("getnameinfo() error");
+  if (ret > 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
     exit(1);
   }
 
   if (result->ai_family != AF_INET) {
-    fprintf(stderr, "IPv6 addresses not handled in this example.\n");
-    free(ip_str);
+    fprintf(stderr, "IPv6 addresses not handled\n");
     freeaddrinfo(result);
     exit(1);
   }
 
-  struct sockaddr_in *ipv4 = (struct sockaddr_in *)result->ai_addr;
+  char *ip_str = malloc(NI_MAXHOST);
+
+  if (!ip_str) {
+    freeaddrinfo(result);
+    perror("malloc() error");
+    exit(1);
+  }
+
+  t_sockaddr_in *ipv4 = (t_sockaddr_in *)result->ai_addr;
   inet_ntop(AF_INET, &(ipv4->sin_addr), ip_str, NI_MAXHOST);
 
   freeaddrinfo(result);
--- src/utils/time.c
+++ src/utils/time.c
@@ -1,13 +1,14 @@
-#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #include "ft_ping.h"
 
-double timespecToMs(struct timespec t) {
-  return t.tv_sec * 1000.0 + t.tv_nsec / 1000000.0;
+double timespecToMs(t_timespec tv) {
+  return tv.tv_sec * 1000.0 + tv.tv_nsec / 1000000.0;
 }
 
-const struct timespec setTime(const int32_t sec, const int32_t nsec) {
+const t_timespec setTime(const int32_t sec, const int32_t nsec) {
   struct timespec tv;
 
   tv.tv_sec = sec;
@@ -15,9 +16,41 @@ const struct timespec setTime(const int32_t sec, const int32_t nsec) {
   return tv;
 }
 
-void accumulate(struct timespec *total, struct timespec new) {
+void getClock(t_timespec tv) {
+  int32_t i = 0;
+
+  while (i < 3) {
+    if (clock_gettime(CLOCK_MONOTONIC, &tv) == 0) {
+      break;
+    }
+    i++;
+  }
+
+  if (i == 3) {
+    fprintf(stderr, "Failed to get monotonic clock time after %d attempts.\n",
+            3);
+    exit(EXIT_FAILURE);
+  }
+}
+
+const t_timespec setDuration(t_timespec t_start, t_timespec t_end) {
+  t_timespec time = {0, 0};
+
+  if ((t_end.tv_nsec - t_start.tv_nsec) < 0) {
+    time.tv_sec = t_end.tv_sec - t_start.tv_sec - 1;
+    time.tv_nsec = 1000000000 + t_end.tv_nsec - t_start.tv_nsec;
+  } else {
+    time.tv_sec = t_end.tv_sec - t_start.tv_sec;
+    time.tv_nsec = t_end.tv_nsec - t_start.tv_nsec;
+  }
+
+  return time;
+}
+
+void accumulate(t_timespec *total, t_timespec new) {
   total->tv_sec += new.tv_sec;
   total->tv_nsec += new.tv_nsec;
+
   if (total->tv_nsec >= 1000000000) {
     total->tv_sec++;
     total->tv_nsec -= 1000000000;
